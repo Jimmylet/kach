0b8f61c30f5278ad7dc8a5386f2c76e5c35740c4
Jimmy Letecheur <jimmy@letecheur.me>
2016-10-21 10:07:50 +0200

    ignore vagrant tmp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@
 
 var gulp = require( "gulp" ),
     gESLint = require( "gulp-eslint" ),
-    gBabel = require( "gulp-babel" );
+    gBabel = require( "gulp-babel" ),
+    gUtil = require( "gulp-util" ),
+    Mongo = require( "mongodb" ),
+    ObjectID = Mongo.ObjectID,
+    MongoClient = Mongo.MongoClient;
 
 gulp.task( "lint", function() {
     return gulp
@@ -17,6 +21,19 @@ gulp.task( "build", function() {
         .pipe( gBabel() )
         .pipe( gulp.dest( "bin" ) )
 } );
+
+gulp.task( "reset-db", function ( fNext ) {
+    if ( process.env.USER !== "vagrant" ){
+        gUtil.beep();
+        gUtil.log( gUtil.colors.red( "This task must be runned from INSIDE the vagrant box!"));
+        return fNext();
+    }
+    console.log("kikoo");
+    // 1. Verify that we are INSIDE the vagrant
+    // 2. drop databases
+    // 3. parse & fill banks
+    // 4. parse & fill terminals
+});
 
 gulp.task( "watch", function() {
     gulp.watch( "src/**/*.js", [ "build" ] );
