d4f81599d6b4c3229575370a14968836013ec649
Jimmy Letecheur <jimmy@letecheur.me>
2016-10-21 09:47:04 +0200

    Init project & tasks

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var gulp = require( "gulp" ),
+    gESLint = require( "gulp-eslint" ),
+    gBabel = require( "gulp-babel" );
+
+gulp.task( "lint", function() {
+    return gulp
+        .src( "src/**/*.js" )
+        .pipe( gESLint() )
+        .pipe( gESLint.format() );
+} );
+
+gulp.task( "build", function() {
+    return gulp
+        .src( "src/**/*.js" )
+        .pipe( gBabel() )
+        .pipe( gulp.dest( "bin" ) )
+} );
+
+gulp.task( "watch", function() {
+    gulp.watch( "src/**/*.js", [ "build" ] );
+} );
+
+gulp.task( "default", [ "build" ] );
+
+gulp.task( "work", [ "build", "watch" ] );
